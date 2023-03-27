ebaeba9f96e43dc4cc40ac89558f5296fcdcd2f9
shahid <shahidazam574@gmail.com>
2023-03-27 12:01:14 +0530

    added  address module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { Global, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { mongoConfig, mongoConfig2 } from 'src/config/mongodb-connection';
+import { AddressModule } from 'src/modules/address/address.module';
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { DomainModule } from 'src/modules/domain/domain.module';
 import { DbModule } from 'src/modules/mongoose/mongoose.module';
@@ -11,31 +12,12 @@ import { TenantModule } from 'src/modules/tenant/tenant.module';
 ConfigModule.forRoot({ isGlobal: true });
 @Module({
   imports: [
-    MongooseModule.forRootAsync({
-      useFactory(config: ConfigService) {
-        let uri = mongoConfig2().MONGO_URI;
-        return {
-          uri,
-        };
-      },
-      connectionName: 'admin1',
-      inject: [ConfigService],
-    }),
-    // MongooseModule.forRootAsync({
-    //   useFactory(config: ConfigService) {
-    //     let uri = mongoConfig().MONGO_URI;
-    //     return {
-    //       uri,
-    //     };
-    //   },
-    //   connectionName: 'profile',
-    //   inject: [ConfigService],
-    // }),
     DbModule,
     TenantModule,
     DomainModule,
     AuthModule,
     ProfileModule,
+    AddressModule,
   ],
 })
 export class AppModule {}
